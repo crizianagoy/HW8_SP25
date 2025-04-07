57b1946497e684297e73d1c780fc6b75cbe040e8
crizianagoy <cnagoy@okstate.edu>
2025-04-06 21:56:45 -0500

    initial

--- P1/Pump_MVC.py
+++ P1/Pump_MVC.py
@@ -1,4 +1,4 @@
-#region imorts
+# region imorts
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
@@ -6,15 +6,18 @@ import PyQt5.QtWidgets as qtw
 
 # importing from previous work on least squares fit
 from LeastSquares import LeastSquaresFit_Class
-#endregion
 
-#region class definitions
+
+# endregion
+
+# region class definitions
 class Pump_Model():
     """
     This is the pump model.  It just stores data.
     """
-    def __init__(self): #pump class constructor
-        #create some class variables for storing information
+
+    def __init__(self):  # pump class constructor
+        # create some class variables for storing information
         self.PumpName = ""
         self.FlowUnits = ""
         self.HeadUnits = ""
@@ -29,83 +32,90 @@ class Pump_Model():
         self.EfficiencyCoefficients = np.array([])
 
         # create two instances (objects) of least squares class
-        self.LSFitHead=LeastSquaresFit_Class()
-        self.LSFitEff=LeastSquaresFit_Class()
+        self.LSFitHead = LeastSquaresFit_Class()
+        self.LSFitEff = LeastSquaresFit_Class()
+
 
 class Pump_Controller():
     def __init__(self):
         self.Model = Pump_Model()
         self.View = Pump_View()
-    
-    #region functions to modify data of the model
+
+    # region functions to modify data of the model
     def ImportFromFile(self, data):
         """
         This processes the list of strings in data to build the pump model
-        :param data: 
-        :return: 
+        :param data:
+        :return:
         """
-        self.Model.PumpName = #JES Missing Code
-        #data[1] is the units line
-        L=data[2].split()
-        self.Model.FlowUnits = #JES Missing Code
-        self.Model.HeadUnits = #JES Missing Code
+        self.Model.PumpName = data[0].strip()
+        # data[1] is the units line
+        L = data[2].split()
+        self.Model.FlowUnits = L[0]
+        self.Model.HeadUnits = L[1]
 
         # extracts flow, head and efficiency data and calculates coefficients
         self.SetData(data[3:])
         self.updateView()
-    
-    def SetData(self,data):
+
+    def SetData(self, data):
         '''
         Expects three columns of data in an array of strings with space delimiter
         Parse line and build arrays.
         :param data:
         :return:
         '''
-        #erase existing data
+        # erase existing data
         self.Model.FlowData = np.array([])
         self.Model.HeadData = np.array([])
         self.Model.EffData = np.array([])
 
-        #parse new data
+        # parse new data
         for L in data:
-            Cells=#JES Missing Code #parse the line into an array of strings
-            self.Model.FlowData=np.append(self.Model.FlowData, #JES Missing Code) #remove any spaces and convert string to a float
-            self.Model.HeadData=np.append(self.Model.HeadData, #JES Missing Code) #remove any spaces and convert string to a float
-            self.Model.EffData=np.append(self.Model.EffData, #JES Missing Code) #remove any spaces and convert string to a float
+            Cells = L.strip().split()  # parse the line into an array of strings
+            self.Model.FlowData = np.append(self.Model.FlowData,
+                                            float(Cells[0]))  # remove any spaces and convert string to a float
+            self.Model.HeadData = np.append(self.Model.HeadData,
+                                            float(Cells[1]))  # remove any spaces and convert string to a float
+            self.Model.EffData = np.append(self.Model.EffData,
+                                           float(Cells[2]))  # remove any spaces and convert string to a float
 
-        #call least square fit for head and efficiency
+        # call least square fit for head and efficiency
         self.LSFit()
-        
+
     def LSFit(self):
         '''Fit cubic polynomial using Least Squares'''
-        self.Model.LSFitHead.x=self.Model.FlowData
-        self.Model.LSFitHead.y=self.Model.HeadData
-        self.Model.LSFitHead.LeastSquares(3) #calls LeastSquares function of LSFitHead object
+        self.Model.LSFitHead.x = self.Model.FlowData
+        self.Model.LSFitHead.y = self.Model.HeadData
+        self.Model.LSFitHead.LeastSquares(3)  # calls LeastSquares function of LSFitHead object
 
-        self.Model.LSFitEff.x=self.Model.FlowData
-        self.Model.LSFitEff.y=self.Model.EffData
-        self.Model.LSFitEff.LeastSquares(3) #calls LeastSquares function of LSFitEff object
-    #endregion
+        self.Model.LSFitEff.x = self.Model.FlowData
+        self.Model.LSFitEff.y = self.Model.EffData
+        self.Model.LSFitEff.LeastSquares(3)  # calls LeastSquares function of LSFitEff object
 
-    #region functions interacting with view
+    # endregion
+
+    # region functions interacting with view
     def setViewWidgets(self, w):
         self.View.setViewWidgets(w)
 
     def updateView(self):
         self.View.updateView(self.Model)
-    #endregion
+    # endregion
+
+
 class Pump_View():
     def __init__(self):
         """
         In this constructor, I create some QWidgets as placeholders until they get defined later.
         """
-        self.LE_PumpName=qtw.QLineEdit()
-        self.LE_FlowUnits=qtw.QLineEdit()
-        self.LE_HeadUnits=qtw.QLineEdit()
-        self.LE_HeadCoefs=qtw.QLineEdit()
-        self.LE_EffCoefs=qtw.QLineEdit()
-        self.ax=None
-        self.canvas=None
+        self.LE_PumpName = qtw.QLineEdit()
+        self.LE_FlowUnits = qtw.QLineEdit()
+        self.LE_HeadUnits = qtw.QLineEdit()
+        self.LE_HeadCoefs = qtw.QLineEdit()
+        self.LE_EffCoefs = qtw.QLineEdit()
+        self.ax = None
+        self.canvas = None
 
     def updateView(self, Model):
         """
@@ -130,11 +140,25 @@ class Pump_View():
         effx, effy, effRSq = Model.LSFitEff.GetPlotInfo(3, npoints=500)
 
         axes = self.ax
-        #JES Missing code (many lines to make the graph)
+        axes.clear()
+
+        # Plot Head Curve
+        axes.plot(Model.FlowData, Model.HeadData, 'bo', label='Head Data')
+        axes.plot(headx, heady, 'b-', label=f'Head Fit (R²={headRSq:.3f})')
+
+        # Plot Efficiency Curve
+        axes.plot(Model.FlowData, Model.EffData, 'go', label='Efficiency Data')
+        axes.plot(effx, effy, 'g-', label=f'Efficiency Fit (R²={effRSq:.3f})')
+
+        axes.set_title(f"Pump Curve: {Model.PumpName}")
+        axes.set_xlabel(f"Flow ({Model.FlowUnits})")
+        axes.set_ylabel(f"Head / Efficiency ({Model.HeadUnits}, -)")
+        axes.grid(True)
+        axes.legend()
 
         self.canvas.draw()
 
     def setViewWidgets(self, w):
         self.LE_PumpName, self.LE_FlowUnits, self.LE_HeadUnits, self.LE_HeadCoefs, self.LE_EffCoefs, self.ax, self.canvas = w
-#endregion
+# endregion
 
